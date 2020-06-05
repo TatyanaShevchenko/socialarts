12904bfbe0e931df617e11d5df8bc3e2d6a44253
Tatyana Shevchenko <dafna2011@gmail.com>
2020-06-05 13:40:21 +0300

    add form for dialogs

--- src/components/Dialogs/SendMessage/SendMessage.jsx
+++ src/components/Dialogs/SendMessage/SendMessage.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import style from "./SendMessage.module.css";
+import {Field, reduxForm} from "redux-form";
+
+const SendMessageForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} className={style.addMessage}>
+            <Field placeholder={"Enter your message"} name={"newMessageBody"} component={"textarea"}/>
+            <button>Send</button>
+            {/*<textarea onChange={onNewMessageChange} value={props.newMessageText} placeholder='Enter your message'/>*/}
+            {/*<button onClick={onSendMessageClick}>Send</button>*/}
+        </form>
+    )
+}
+
+const ReduxSendMessageForm = reduxForm({form: 'sendMessage'})(SendMessageForm)
 
 const SendMessage = (props) => {
-
     let onSendMessageClick = () => {
         props.sendMessage();
     };
-
     let onNewMessageChange = (e) => {
         let text = e.target.value;
         props.updateNewMessageText(text);
     };
-
+    const addNewMessage = (values) => {
+        // alert(values.newMessageBody);
+        props.sendMessage(values.newMessageBody);
+    }
     return (
-        <div className={style.addMessage}>
-            <textarea onChange={onNewMessageChange} value={props.newMessageText} placeholder='Enter your message'/>
-            <button onClick={onSendMessageClick}>Send</button>
-        </div>
-
+        <ReduxSendMessageForm onSubmit={addNewMessage}/>
     )
 }
 
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -3,54 +3,53 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 let initialState = {
     dialogs: [
-        { id: 1, name: 'Tanya', photo: 'https://sun9-53.userapi.com/c846121/v846121902/e60ad/jPAFoHaGCCY.jpg' },
-        { id: 2, name: 'Alex', photo: 'https://sun9-29.userapi.com/c639323/v639323592/3a0c8/3l9NMzf5GwA.jpg' },
-        { id: 3, name: 'Owen', photo: 'https://sun9-7.userapi.com/c626929/v626929955/35a8b/XyiKoZdUzII.jpg' },
-        { id: 4, name: 'Ann', photo: 'https://sun9-11.userapi.com/c850024/v850024490/125ff8/wOogYeVHxXs.jpg' },
-        { id: 5, name: 'Carlie', photo: 'https://sun9-49.userapi.com/c848416/v848416316/11584f/BClLlSomafs.jpg' }
+        {id: 1, name: 'Tanya', photo: 'https://sun9-53.userapi.com/c846121/v846121902/e60ad/jPAFoHaGCCY.jpg'},
+        {id: 2, name: 'Alex', photo: 'https://sun9-29.userapi.com/c639323/v639323592/3a0c8/3l9NMzf5GwA.jpg'},
+        {id: 3, name: 'Owen', photo: 'https://sun9-7.userapi.com/c626929/v626929955/35a8b/XyiKoZdUzII.jpg'},
+        {id: 4, name: 'Ann', photo: 'https://sun9-11.userapi.com/c850024/v850024490/125ff8/wOogYeVHxXs.jpg'},
+        {id: 5, name: 'Carlie', photo: 'https://sun9-49.userapi.com/c848416/v848416316/11584f/BClLlSomafs.jpg'}
     ],
     messages: [
-        { id: 1, message: 'Hi!' },
-        { id: 2, message: 'How are you?' },
-        { id: 3, message: 'Nice to meet you' },
-        { id: 4, message: 'Yo' },
-        { id: 5, message: 'Yoooooooooy!' }
-    ],
-    newMessageText: ''
+        {id: 1, message: 'Hi!'},
+        {id: 2, message: 'How are you?'},
+        {id: 3, message: 'Nice to meet you'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yoooooooooy!'}
+    ]
 };
 
 const dialogsReducer = (state = initialState, action) => {
 
     switch (action.type) {
 
-        case SEND_MESSAGE:
-            {
-
-                return {
-                    ...state,
-                    messages: [...state.messages, {
-                        id: state.messages + 1,
-                        message: state.newMessageText
-                    }],
-                    newMessageText: ''
-                }
+        case SEND_MESSAGE: {
+            let body = action.newMessageBody;
+            return {
+                ...state,
+                messages: [...state.messages, {
+                    id: state.messages + 1,
+                    message: body
+                }]
             }
+        }
 
 
-        case UPDATE_NEW_MESSAGE_TEXT:
-            {
-                return {
-                    ...state,
-                    newMessageText: action.text
-                }
+        case UPDATE_NEW_MESSAGE_TEXT: {
+            return {
+                ...state,
+                newMessageText: action.text
             }
+        }
 
         default:
             return state;
     }
 };
 
-export const sendMessage = () => ({ type: SEND_MESSAGE });
+export const sendMessage = (newMessageBody) => ({
+    type: SEND_MESSAGE,
+    newMessageBody: newMessageBody
+});
 export const updateNewMessageText = (text) => ({
     type: UPDATE_NEW_MESSAGE_TEXT,
     text: text
