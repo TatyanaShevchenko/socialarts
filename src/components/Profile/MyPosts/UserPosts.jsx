512aabe8f0afdac7207fdf6a0d362795765de12a
Tatyana Shevchenko <dafna2011@gmail.com>
2020-06-09 17:09:06 +0300

    react.memo

--- src/components/Profile/MyPosts/UserPosts.jsx
+++ src/components/Profile/MyPosts/UserPosts.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import style from "./UserPosts.module.css";
 import Post from "./Post/Post";
-import AddNewPost from "./AddNewPost/AddNewPost";
 import AddNewPostContainer from "./AddNewPost/AddNewPostContainer";
 
 
-const UserPosts = (props) => {
+const UserPosts = React.memo(props => {
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
     let ReverseFunc = (Elements) => {
         const length = Elements.length;
-        let Reverse =[];
+        let Reverse = [];
         let j = 0;
-        for (let i = length-1; i>=0; i--){
-            Reverse[j]=Elements[i];
+        for (let i = length - 1; i >= 0; i--) {
+            Reverse[j] = Elements[i];
             j += 1;
         }
         return Reverse;
@@ -26,6 +25,6 @@ const UserPosts = (props) => {
             {postsReverseElements}
         </div>
     );
-};
+}) ;
 
 export default UserPosts;
